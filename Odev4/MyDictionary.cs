dc76e0f700ae9f23ff53b93bff37743e06e5d7e5
Kübra Gülmez <kkubragulmez@gmail.com>
2021-01-27 21:27:48 +0300

    Add project files.

---
+++ Odev4/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev4
+{
+    class MyDictionary<Barcode,Name>
+    {
+        Barcode[] barcode;
+        Name[] name;
+        public MyDictionary()
+        {
+            barcode = new Barcode[0];
+            name = new Name[0];
+        }
+
+        public int Length { get; internal set; }
+
+        public void Add(Barcode item1, Name item2)
+        {
+            Barcode[] tempBarcode = barcode;
+            Name[] tempName = name;
+            barcode = new Barcode[barcode.Length + 1];
+            name = new Name[name.Length + 1];
+
+            for (int i = 0; i < tempBarcode.Length; i++)
+            {
+                barcode[i] = tempBarcode[i];
+            }
+            
+
+            for (int i = 0; i < tempName.Length; i++)
+            {
+                name[i] = tempName[i];
+            }
+            barcode[barcode.Length - 1] = item1;
+            name[name.Length - 1] = item2;
+
+            
+
+
+    }
+    }
+}
---
+++ Odev4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Odev4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> dictionary = new MyDictionary<int,string>();
+            dictionary.Add(456789, "Elma");
+            dictionary.Add(829892, "Baharat");
+            dictionary.Add(329894, "Et");
+            dictionary.Add(233890, "Kitap");
+
+            
+        }
+    }
+}
